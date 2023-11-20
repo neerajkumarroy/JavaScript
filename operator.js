f1d33bda17b3061d4dd6f438c2e661578624649c
Neeraj Kumar <148259510+neerajkumarroy@users.noreply.github.com>
2023-11-20 17:06:00 +0530

    all-operators_example

---
+++ operator.js
@@ -0,0 +1,55 @@
+// Arithmetic Operators
+let sum = 5 + 3; // sum is 8
+let difference = 8 - 3; // difference is 5
+let product = 4 * 6; // product is 24
+let quotient = 10 / 2; // quotient is 5
+let remainder = 15 % 4; // remainder is 3
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
+console.log(remainder);
+
+// Assignment Operators
+let x = 10;
+let y = 5;
+y += 3; // y is now 8
+let z = 7;
+z -= 2; // z is now 5
+console.log(y);
+console.log(z);
+
+// Comparison Operators
+let a = 5;
+let b = "5";
+console.log(a == b); // true (loose equality)
+console.log(a === b); // false (strict equality)
+
+let c = 10;
+let d = "10";
+console.log(c != d); // false (loose inequality)
+console.log(c !== d); // true (strict inequality)
+
+let e = 8;
+let f = 5;
+console.log(e > f); // true
+
+let g = 3;
+let h = 6;
+console.log(g < h); // true
+
+// Logical Operators
+let i = true;
+let j = false;
+console.log(i && j); // false
+console.log(i || j); // true
+console.log(!i); // false
+
+// Unary Operators
+let n = 3;
+n++;
+console.log(n); // 4
+
+let p = 7;
+p--;
+console.log(p); // 6
